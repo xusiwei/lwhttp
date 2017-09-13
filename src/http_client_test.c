2c8268dc3f46d06babde161823276bce1beebae5
Siwei Xu <xusiwei1236@gmail.com>
2017-09-13 18:36:23 +0800

    add HTTP POST test

--- src/http_client_test.c
+++ src/http_client_test.c
@@ -3,6 +3,38 @@
 
 char buffer[4096];
 
+void http_post_test()
+{
+    http_client client;
+    http_message request, response;
+
+    http_message_init(&request);
+    http_message_init(&response);
+
+    http_client_init(&client, &request);
+    http_client_set_request_buffer(&client, buffer, sizeof(buffer));
+    http_client_set_response_buffer(&client, buffer, sizeof(buffer));
+
+    http_message_set_url(&request, "http://emdroid.org:9090/");
+    http_message_set_method(&request, HTTP_POST);
+    http_message_add_parameter(&request, "temp", "23");
+
+    http_client_execute(&client, &response);
+
+    printf("==== response header ====\n");
+    for (int i = 0; i < response.nheaders; i++) {
+        static char key[64], val[64];
+        ss_strcopy(key, sizeof(key), response.header_names[i]);
+        ss_strcopy(val, sizeof(val), response.header_values[i]);
+        printf("%s: %s\n", key, val);
+    }
+
+    static char body[4096];
+    ss_strcopy(body, sizeof(body), response.body);
+    printf("==== response body ====\n%s", body);
+    printf("=======================\n");
+}
+
 int main(int argc, char* argv[])
 {
     const char* url = "http://example.com/";
@@ -50,5 +82,7 @@ int main(int argc, char* argv[])
     ss_strcopy(body, sizeof(body), response.body);
     printf("==== response body ====\n%s", body);
     printf("=======================\n");
+
+    http_post_test();
     return 0;
 }
